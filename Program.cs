2229f7d8b3495d5e3a938f133a1be55dedd7d7c8
Alftron <mja.haycock@gmail.com>
2023-02-12 20:04:05 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+/*
+ * Flip a coin the number of times entered by the user and print total heads and tails
+ */
+
+namespace CoinFlipper
+{
+    class Program
+    {
+        enum CoinSides
+        {
+            Heads,
+            Tails
+        }
+        
+        static void Main(string[] args)
+        {
+            // Create random number object
+            var rand = new Random();
+            // Valid input flag
+            bool validInput = false;
+
+            // Introduce the program and get the number
+            do
+            {
+                Console.WriteLine("Flip a coin!");
+                Console.WriteLine("Enter the number of coin flips you would like: ");
+                string input = Console.ReadLine();
+                // Check the input was a number
+                if (int.TryParse(input, out int value))
+                {
+                    // Value is safe to use here
+                    validInput = true;
+                    // Track the values
+                    int heads = 0;
+                    int tails = 0;
+                        
+                    int i = 0;
+                    while (i < value)
+                    {
+                        int flip = rand.Next(2);
+                        if (flip == (int)CoinSides.Heads)
+                        {
+                            heads++;
+                        }
+                        else
+                        {
+                            tails++;
+                        }
+                        i++;
+                    }
+                    Console.WriteLine("Heads flipped: " + heads);
+                    Console.WriteLine("Tails flipped: " + tails);
+                }
+            } while (!validInput);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# FibonacciSequence
+
+Simple C# program to flip a coin X number of times provided by the user and print the number of heads and tails<br>
+Idea for the project was taken from [Karan's Mega Project List](https://github.com/karan/Projects)
